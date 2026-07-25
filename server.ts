4387975623aca82b89997f122586c282a70edf02
jsanderstechnologies <jsanderstechnologies@gmail.com>
2026-07-25 09:57:33 -0500

    Support Intel i915 VAAPI & QSV hardware video transcoding in server

--- server.ts
+++ server.ts
@@ -105,7 +105,8 @@ function detectBestH264Encoder(): string {
   if (bestH264Encoder !== null) return bestH264Encoder;
   try {
     const { execSync } = require('child_process');
-    const encoders = ['h264_nvenc', 'h264_qsv', 'h264_amf', 'h264_videotoolbox'];
+    const encoders = ['h264_qsv', 'h264_vaapi', 'h264_nvenc', 'h264_amf', 'h264_videotoolbox'];
+
     for (const enc of encoders) {
       try {
         execSync(`"${ffmpegPath}" -f lavfi -i nullsrc=s=1280x720 -c:v ${enc} -t 1 -f null -`, {stdio: 'ignore'});
