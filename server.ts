a854795ad1c7e47fdd42d3241b2f574e388cc03b
Jessie <jsanderstechnologies@gmail.com>
2026-07-28 22:41:31 -0500

    Increase express body-parser payload limit to 50mb

--- server.ts
+++ server.ts
@@ -340,7 +340,7 @@ async function startServer() {
   const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 5150;
 
   app.set('trust proxy', true);
-  app.use(express.json());
+  app.use(express.json({ limit: '50mb' }));
 
   // IP Logging Middleware
   app.use((req, res, next) => {
